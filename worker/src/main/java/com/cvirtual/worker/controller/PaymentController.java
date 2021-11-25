839943d0f22903df5b23cd7c85b254233d4f7450
Ivan Pagnoncelli <ipagnoncelli@gmail.com>
2021-11-25 17:37:38 -0300

    Organizando imports

--- worker/src/main/java/com/cvirtual/worker/controller/PaymentController.java
+++ worker/src/main/java/com/cvirtual/worker/controller/PaymentController.java
@@ -2,8 +2,6 @@ package com.cvirtual.worker.controller;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import com.cvirtual.worker.entity.Payment;
 import com.cvirtual.worker.service.PaymentService;
 import com.cvirtual.worker.vo.v1.PaymentVO;
@@ -14,10 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
